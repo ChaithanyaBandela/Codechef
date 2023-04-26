772a1052db35bce0e3192f0c64bc61bb8756feee
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-04-26 22:46:05 +0530

    Add files via upload

---
+++ HEIGHTRATION.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b,c;
+	scanf("%d%d",&a,&b);
+	c=a/b;
+	if(a>b)
+	{
+	    printf("%d\n",a);
+	}
+	else
+	{
+	    printf("%d\n",b);
+	}
+	return 0;
+}
+
