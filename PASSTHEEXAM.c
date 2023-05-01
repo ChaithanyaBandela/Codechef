624abb4e425cd683fcae93a922e7d5c6fe26842f
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-01 21:49:59 +0530

    Add files via upload

---
+++ PASSTHEEXAM.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b,c;
+	    scanf("%d%d%d",&a,&b,&c);
+	    if((a+b+c)>=100 && a>=10 && b>=10 && c>=10)
+	    {
+	        printf("PASS\n");
+	    }
+	    else
+	    {
+	        printf("FAIL\n");
+	    }
+	}
+	return 0;
+}
+
