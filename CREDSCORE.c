a7a90bd0de80979e12e21286a5d0f2312b4103b6
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-05 21:58:32 +0530

    Add files via upload

---
+++ CREDSCORE.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	    int x;
+	    scanf("%d",&x);
+	    if(x>=750)
+	    {
+	        printf("Yes");
+	    }
+	    else
+	    printf("No");
+	return 0;
+}
+
