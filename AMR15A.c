3c9f3887318d969b7e56a5c40a115cea2f49924a
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-05 22:32:09 +0530

    Add files via upload

---
+++ AMR15A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,i,a[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	int x=0,y=0;
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]%2==0)
+	    {
+	        x++;
+	    }
+	    else
+	    {
+	        y++;
+	    }
+	}
+	if(x>y)
+	printf("READY FOR BATTLE");
+	else
+	printf("NOT READY");
+	return 0;
+}
+
---
+++ BATTERYLOW.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x<=15)
+	    printf("Yes\n");
+	    else
+	    printf("No\n");
+	}
+	return 0;
+}
+
