b67ad4e6dd9d0d5b66a726e8e0015ce31e622686
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-06 21:41:28 +0530

    Add files via upload

---
+++ TRUESCORE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b,c,d;
+	    scanf("%d%d%d%d",&a,&b,&c,&d);
+	    if(c>=a && d>=b )
+	    {
+	        printf("POSSIBLE\n");
+	    }
+	    else
+	    {
+	        printf("IMPOSSIBLE\n");
+	    }
+	}
+	return 0;
+}
+
