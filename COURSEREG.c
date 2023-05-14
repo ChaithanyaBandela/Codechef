bf75dc65c5fd39a6c03852fa20df026d24bdcae6
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-14 16:08:19 +0530

    Add files via upload

---
+++ COURSEREG.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n,m,k;
+	    scanf("%d%d%d",&n,&m,&k);
+	    if(n+k<=m)
+	    {
+	        printf("YEs\n");
+	    }
+	    else
+	    printf("No\n");
+	}
+	return 0;
+}
+
