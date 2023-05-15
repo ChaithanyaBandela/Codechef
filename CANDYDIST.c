98a9d54ac8ea57d6ba9f917b428fcaf2dde560fe
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-15 20:20:34 +0530

    Add files via upload

---
+++ CANDYDIST.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    if((x/y)%2==0 && x%y==0)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
