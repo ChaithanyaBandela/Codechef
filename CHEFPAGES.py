2d09bb3d5e6ed942747779dafece6dd3ecb40b88
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-05-24 16:04:16 +0530

    Add files via upload

---
+++ CHEFPAGES.py
@@ -0,0 +1,8 @@
+# cook your dish here
+A,B=map(int,input().split())
+if A==0 :
+    print('https://www.codechef.com/practice')
+elif B==0:
+    print('https://www.codechef.com/contests')
+else:
+    print('https://discuss.codechef.com')
