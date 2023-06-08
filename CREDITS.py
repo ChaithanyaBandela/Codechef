3d185b785307b649f2c592c2b0e930bbd439fe90
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-06-08 20:38:58 +0530

    Add files via upload

---
+++ CREDITS.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    a=int(input())
+    if a>65:
+        print('Overload')
+    elif a<35:
+        print('Underload')
+    else:
+        print('Normal')
