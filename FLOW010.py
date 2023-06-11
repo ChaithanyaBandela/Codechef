5096f2502a123a48ef03d11f954dee4dbcd10cdb
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-06-11 21:20:36 +0530

    Add files via upload

---
+++ FLOW010.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for _ in range(int(input())):
+    a=input()
+    if a=='B' or a=='b':
+        print('BattleShip')
+    elif a=='C' or a=='c':
+        print('Cruiser')
+    elif a=='D' or a=='d':
+        print('Destroyer')
+    elif a=='F' or a=='f':
+        print('Frigate')
