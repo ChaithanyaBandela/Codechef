ad3804b2a526f9dd1c76ecafd7a068874d2cb217
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-06-13 14:48:02 +0530

    Add files via upload

---
+++ PROBCAT.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    x=int(input())
+    if 1<=x<100:
+        print('Easy')
+    elif 100<=x<200:
+        print('Medium')
+    elif 200<=x<=300:
+        print('Hard')
