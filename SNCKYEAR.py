06a24ffab59e5b66a1b59362920980d2d2b66935
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-06-30 21:54:05 +0530

    Add files via upload

---
+++ SNCKYEAR.py
@@ -0,0 +1,15 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    if n==2010:
+        print('HOSTED')
+    elif n==2015:
+        print('HOSTED')
+    elif n==2016:
+        print('HOSTED')
+    elif n==2017:
+        print('HOSTED')
+    elif n==2019:
+        print('HOSTED')
+    else:
+        print('NOT HOSTED')
