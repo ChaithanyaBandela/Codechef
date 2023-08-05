6637c6ab075e70f27308bac49a8247489dbd9809
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-08-05 21:28:44 +0530

    Add files via upload

---
+++ TRANSFORM.py
@@ -0,0 +1,10 @@
+# cook your dish here
+for i in range(int(input())):
+    n=int(input())
+    if n%3==1:
+        print('HUGE')
+    elif n%3==2:
+        print('SMALL')
+    else:
+        print('NORMAL')
+        
