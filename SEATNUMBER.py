1cb926bcd65566a5bb04f7603eb261b98f9a2c13
ChaithanyaBandela <119913307+ChaithanyaBandela@users.noreply.github.com>
2023-08-22 21:40:14 +0530

    Add files via upload

--- SEATNUMBER.py
+++ SEATNUMBER.py
@@ -1,14 +1,11 @@
 # cook your dish here
-# cook your dish here
 for i in range(int(input())):
     n=int(input())
-    if(n<16):
-        if(n<11):
-            print('Lower Double')
-        else:
-            print('Lower Single')
-    else:
-        if(n<26):
-            print('Upper Double')
-        else:
-            print('Upper Single')
+    if 1<=n<=10:
+        print('Lower Double')
+    elif 11<=n<=15:
+        print('Lower Single')
+    elif 16<=n<=25:
+        print('Upper Double')
+    elif 26<=n<=30:
+        print('Upper Single')
